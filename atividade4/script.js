8c3fe79c958f8fc9d788b6d14e6cab60de6f8c52
bernard-silva <75813997+bernard-silva@users.noreply.github.com>
2024-04-06 20:29:19 -0300

    refactor/fix: refatorado js com funções / alterado para o icone do player principal do rodapé também alterar quando clicado no ícone de play e pause do video, porém player principal ainda não está funcional

--- atividade4/script.js
+++ atividade4/script.js
@@ -1,3 +1,7 @@
+let btnVideo = document.getElementById('btnVideo')
+let videoPlayer = document.getElementById('videoPlayer')
+let relative = document.querySelector('.relative')
+
 let btnAudio1 = document.getElementById('btnAudio1')
 let btnAudio2 = document.getElementById('btnAudio2')
 let btnAudio3 = document.getElementById('btnAudio3')
@@ -8,6 +12,10 @@ let audio3 = document.getElementById('audio3')
 
 let btnMainPlay = document.getElementById('btnMainPlay')
 
+// VIDE #OBSERVACAO - (START)
+let showControls = document.getElementById('showControls')
+let hideControls = document.getElementById('hideControls')
+
 let showControls1 = document.getElementById('showControls1')
 let hideControls1 = document.getElementById('hideControls1')
 
@@ -16,226 +24,153 @@ let hideControls2 = document.getElementById('hideControls2')
 
 let showControls3 = document.getElementById('showControls3')
 let hideControls3 = document.getElementById('hideControls3')
+// VIDE #OBSERVACAO - (END)
 
-let btnVideo = document.getElementById('btnVideo')
-let videoPlayer = document.getElementById('videoPlayer')
-let showControls = document.getElementById('showControls')
-let hideControls = document.getElementById('hideControls')
+const playIcon = '<i class="fa-solid fa-circle-play fa-2x"></i>'
+const pauseIcon = '<i class="fas fa-pause-circle fa-2x"></i>'
 
-let relative = document.querySelector('.relative')
+function addPlayIcon(btnMedia) {
+    btnMedia.innerHTML = playIcon
+    btnMedia.classList.remove('playing')
+    btnMedia.classList.add('paused')
+    btnMainPlay.innerHTML = playIcon
+}
 
-showControls.addEventListener('click', () => {
-    videoPlayer.setAttribute('controls', true)
-    showControls.style.display = 'none'
-    hideControls.style.display = 'inline'
-    btnVideo.style.display = 'none'
-})
+function pauseAll() {
+    videoPlayer.pause()
+    audio1.pause()
+    audio2.pause()
+    audio3.pause()
 
-hideControls.addEventListener('click', () => {
-    videoPlayer.removeAttribute('controls')
-    hideControls.style.display = 'none'
-    showControls.style.display = 'inline'
-    btnVideo.style.display = 'block'
-})
+    let arrBtnMedia = [btnVideo, btnAudio1, btnAudio2, btnAudio3]
+
+    arrBtnMedia.forEach(btnMedia => {
+        addPlayIcon(btnMedia)
+    });
+}
+
+function playMedia(media, btnMedia) {
+    media.play()
+    btnMedia.innerHTML = pauseIcon
+    btnMedia.classList.remove('paused')
+    btnMedia.classList.add('playing')
+    btnMainPlay.innerHTML = pauseIcon
+}
+
+function pauseMedia(media, btnMedia) {
+    media.pause()
+    addPlayIcon(btnMedia)
+}
 
 btnVideo.addEventListener('click', () => {
-    // console.log('clicou')
     if (btnVideo.classList.value == 'paused') {
-        audio1.pause()
-        audio2.pause()
-        audio3.pause()
-        btnAudio1.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnAudio1.classList.remove('playing')
-        btnAudio1.classList.add('paused')
-        btnAudio2.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnAudio2.classList.remove('playing')
-        btnAudio2.classList.add('paused')
-        btnAudio3.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnAudio3.classList.remove('playing')
-        btnAudio3.classList.add('paused')
-
-        videoPlayer.play()
-        btnVideo.innerHTML = '<i class="fas fa-pause-circle fa-2x"></i>'
-        btnVideo.classList.remove('paused')
-        btnVideo.classList.add('playing')
+        pauseAll()
+        playMedia(videoPlayer, btnVideo)
         btnVideo.style.opacity = 0
-        // console.log('dentro if')
     } else {
-        videoPlayer.pause()
-        btnVideo.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnVideo.classList.remove('playing')
-        btnVideo.classList.add('paused')
-        // console.log('dentro else')
+        pauseMedia(videoPlayer, btnVideo)
     }
-})
-
-relative.addEventListener('mouseenter', () => {
-    btnVideo.style.opacity = 0.8
 })
 
 relative.addEventListener('mouseleave', () => {
     btnVideo.style.opacity = 0
 })
 
+relative.addEventListener('mouseenter', () => {
+    btnVideo.style.opacity = 0.8
+})
+
 btnVideo.addEventListener('mouseenter', () => {
     btnVideo.style.opacity = 0.8
 })
 
-showControls1.addEventListener('click', () => {
-    audio1.setAttribute('controls', true)
-    showControls1.style.display = 'none'
-    hideControls1.style.display = 'inline'
-})
-
-hideControls1.addEventListener('click', () => {
-    audio1.removeAttribute('controls')
-    hideControls1.style.display = 'none'
-    showControls1.style.display = 'inline'
-})
-
-showControls2.addEventListener('click', () => {
-    audio2.setAttribute('controls', true)
-    showControls2.style.display = 'none'
-    hideControls2.style.display = 'inline'
-})
-
-hideControls2.addEventListener('click', () => {
-    audio2.removeAttribute('controls')
-    hideControls2.style.display = 'none'
-    showControls2.style.display = 'inline'
-})
-
-showControls3.addEventListener('click', () => {
-    audio3.setAttribute('controls', true)
-    showControls3.style.display = 'none'
-    hideControls3.style.display = 'inline'
-})
-
-hideControls3.addEventListener('click', () => {
-    audio3.removeAttribute('controls')
-    hideControls3.style.display = 'none'
-    showControls3.style.display = 'inline'
-})
-
 btnAudio1.addEventListener('click', () => {
-    // console.log('clicou')
     if (btnAudio1.classList.value == 'paused') {
-        videoPlayer.pause()
-        btnVideo.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnVideo.classList.remove('playing')
-        btnVideo.classList.add('paused')
-        audio2.pause()
-        audio3.pause()
-        btnAudio2.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnAudio2.classList.remove('playing')
-        btnAudio2.classList.add('paused')
-        btnAudio3.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnAudio3.classList.remove('playing')
-        btnAudio3.classList.add('paused')
-
-        audio1.play()
-        btnAudio1.innerHTML = '<i class="fas fa-pause-circle fa-2x"></i>'
-        btnAudio1.classList.remove('paused')
-        btnAudio1.classList.add('playing')
-        btnMainPlay.innerHTML = '<i class="fas fa-pause-circle fa-2x"></i>'
-        // console.log('dentro if')
+        pauseAll()
+        playMedia(audio1, btnAudio1)
     } else {
-        audio1.pause()
-        btnAudio1.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnAudio1.classList.remove('playing')
-        btnAudio1.classList.add('paused')
-        btnMainPlay.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        // console.log('dentro else')
+        pauseMedia(audio1, btnAudio1)
     }
 })
 
 btnAudio2.addEventListener('click', () => {
-    // console.log('clicou')
     if (btnAudio2.classList.value == 'paused') {
-        videoPlayer.pause()
-        btnVideo.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnVideo.classList.remove('playing')
-        btnVideo.classList.add('paused')
-        audio1.pause()
-        audio3.pause()
-        btnAudio1.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnAudio1.classList.remove('playing')
-        btnAudio1.classList.add('paused')
-        btnAudio3.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnAudio3.classList.remove('playing')
-        btnAudio3.classList.add('paused')
-
-        audio2.play()
-        btnAudio2.innerHTML = '<i class="fas fa-pause-circle fa-2x"></i>'
-        btnAudio2.classList.remove('paused')
-        btnAudio2.classList.add('playing')
-        btnMainPlay.innerHTML = '<i class="fas fa-pause-circle fa-2x"></i>'
-        // console.log('dentro if')
+        pauseAll()
+        playMedia(audio2, btnAudio2)
     } else {
-        audio2.pause()
-        btnAudio2.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnAudio2.classList.remove('playing')
-        btnAudio2.classList.add('paused')
-        btnMainPlay.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        // console.log('dentro else')
+        pauseMedia(audio2, btnAudio2)
     }
 })
 
 btnAudio3.addEventListener('click', () => {
-    // console.log('clicou')
     if (btnAudio3.classList.value == 'paused') {
-        videoPlayer.pause()
-        btnVideo.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnVideo.classList.remove('playing')
-        btnVideo.classList.add('paused')
-        audio1.pause()
-        audio2.pause()
-        btnAudio1.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnAudio1.classList.remove('playing')
-        btnAudio1.classList.add('paused')
-        btnAudio2.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnAudio2.classList.remove('playing')
-        btnAudio2.classList.add('paused')
-
-        audio3.play()
-        btnAudio3.innerHTML = '<i class="fas fa-pause-circle fa-2x"></i>'
-        btnAudio3.classList.remove('paused')
-        btnAudio3.classList.add('playing')
-        btnMainPlay.innerHTML = '<i class="fas fa-pause-circle fa-2x"></i>'
-        // console.log('dentro if')
+        pauseAll()
+        playMedia(audio3, btnAudio3)
     } else {
-        audio3.pause()
-        btnAudio3.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        btnAudio3.classList.remove('playing')
-        btnAudio3.classList.add('paused')
-        btnMainPlay.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-        // console.log('dentro else')
+        pauseMedia(audio3, btnAudio3)
     }
 })
 
 videoPlayer.addEventListener('ended', () => {
-    btnVideo.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-    btnVideo.classList.remove('playing')
-    btnVideo.classList.add('paused')
+    addPlayIcon(btnVideo)
 })
 
 audio1.addEventListener('ended', () => {
-    btnAudio1.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-    btnAudio1.classList.remove('playing')
-    btnAudio1.classList.add('paused')
-    btnMainPlay.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
+    addPlayIcon(btnAudio1)
 })
 
 audio2.addEventListener('ended', () => {
-    btnAudio2.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-    btnAudio2.classList.remove('playing')
-    btnAudio2.classList.add('paused')
-    btnMainPlay.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
+    addPlayIcon(btnAudio2)
 })
 
 audio3.addEventListener('ended', () => {
-    btnAudio3.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
-    btnAudio3.classList.remove('playing')
-    btnAudio3.classList.add('paused')
-    btnMainPlay.innerHTML = '<i class="fa-solid fa-circle-play fa-2x"></i>'
+    addPlayIcon(btnAudio3)
+})
+
+// #OBSERVACAO: MOSTRA OU OCULTA OS CONTROLES DO VIDEO E DOS AUDIOS
+// SOLUÇÃO PALEATIVA DEVIDO A FALTA DA FEATURE DO PLAYER PRINCIPAL E BARRA DE PROGRESSO DO RODAPÉ
+function showMediaControls(media, showControls, hideControls) {
+    media.setAttribute('controls', true)
+    showControls.style.display = 'none'
+    hideControls.style.display = 'inline'
+}
+
+function hideMediaControls(media, hideControls, showControls) {
+    media.removeAttribute('controls')
+    hideControls.style.display = 'none'
+    showControls.style.display = 'inline'
+}
+
+showControls.addEventListener('click', () => {
+    showMediaControls(videoPlayer, showControls, hideControls)
+    btnVideo.style.display = 'none'
+})
+
+hideControls.addEventListener('click', () => {
+    hideMediaControls(videoPlayer, hideControls, showControls)
+    btnVideo.style.display = 'block'
+})
+
+showControls1.addEventListener('click', () => {
+    showMediaControls(audio1, showControls1, hideControls1)
+})
+
+hideControls1.addEventListener('click', () => {
+    hideMediaControls(audio1, hideControls1, showControls1)
+})
+
+showControls2.addEventListener('click', () => {
+    showMediaControls(audio2, showControls2, hideControls2)
+})
+
+hideControls2.addEventListener('click', () => {
+    hideMediaControls(audio2, hideControls2, showControls2)
+})
+
+showControls3.addEventListener('click', () => {
+    showMediaControls(audio3, showControls3, hideControls3)
+})
+
+hideControls3.addEventListener('click', () => {
+    hideMediaControls(audio3, hideControls3, showControls3)
 })
